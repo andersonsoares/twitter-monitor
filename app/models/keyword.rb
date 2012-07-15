d95907804c5dbf6b94ae6c269633a21acf16015e
Anderson Eugênio Ribeiro Soares <aersandersonsoares@gmail.com>
2012-07-15 14:44:02 -0300

    setting up associations between keyword and twitte

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,3 +1,11 @@
 class Keyword < ActiveRecord::Base
   attr_accessible :name
+  
+  has_many :twittes, :dependent => :delete_all
+  
+  #validations
+  validates_presence_of :name, :on => :create, :message => "can't be blank"
+  validates_uniqueness_of :name, :on => :create, :message => "must be unique"
+  
+  
 end
--- app/models/twitte.rb
+++ app/models/twitte.rb
@@ -1,3 +1,9 @@
 class Twitte < ActiveRecord::Base
   attr_accessible :date, :from_user, :from_user_id_str, :id_str, :keyword_id, :profile_image_url, :text
+  
+  belongs_to :keyword
+  
+  validates_uniqueness_of :id_str, :on => :create
+  
+  
 end
