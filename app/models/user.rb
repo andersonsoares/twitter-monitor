d3f123ead72c22ee68d0f9310a0d97702a1a4576
Anderson Eugênio Ribeiro Soares <aersandersonsoares@gmail.com>
2012-07-27 13:07:14 -0300

    removing registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
 	rolify
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :lockable, :timeoutable and :omniauthable :registerable, :recoverable,
+  devise :database_authenticatable, 
+          :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
