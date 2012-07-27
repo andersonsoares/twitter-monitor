ab6df7d3df6ebe4220cd540005936f839ec32169
Anderson Eugênio Ribeiro Soares <aersandersonsoares@gmail.com>
2012-07-27 14:06:21 -0300

    setting up registration again

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable :registerable, :recoverable,
-  devise :database_authenticatable, 
+  devise :database_authenticatable, :registerable,
           :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
